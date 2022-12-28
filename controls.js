63165cb8bb2728d49c08be3b9d657907e29899b3
alankritkadian <alankritkadian@gmail.com>
2022-12-28 14:36:18 +0530

    Initial Commit

---
+++ controls.js
@@ -0,0 +1,55 @@
+class Controls{
+    constructor(type){
+        this.forward = false;
+        this.reverse = false;
+        this.right = false;
+        this.left = false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
+        
+    }
+    #addKeyboardListeners(){
+        document.onkeydown = (event) => {
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+            //console.log(this);
+        }
+        document.onkeyup = (event) => {
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+            //console.log(this);
+        }
+    }
+}
+            
